eb32808f38db5929bfdb6eca1dfb390ab4129198
NiftyCoffee <ztay0013@student.monash.edu>
2024-08-11 20:33:24 +1000

    Added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from './providers';
 import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Providers>
           <Header />
           {children}
+          <Footer />
         </Providers>
       </body>
     </html>
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="bg-light_secondary dark:bg-dark_secondary text-light dark:text-dark text-center p-2 sticky mt-12">
+            <span>Made by niftycoffee, 2024.</span>
+        </footer>
+    )
+}
